89b5afd111a27ea52ab6be40a8460a49e1e6ae50
DamianDiakov <66869147+DamianDiakov@users.noreply.github.com>
2021-12-11 20:56:21 +0200

    Test

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,6 +2,12 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Main function for the project");
+       repeat("Hello there",5);
+    }
+
+    public static void repeat (String sentence, int times){
+        for (int i = 0; i <times; i++) {
+            System.out.println(sentence);
+        }
     }
 }
